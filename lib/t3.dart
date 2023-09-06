a7383d3f9694f66c41f4377b6819c592079a78aa
김봄이 <s2018w26@e-mirim.hs.kr>
2023-09-06 17:00:05 +0900

    add t3

---
+++ lib/t3.dart
@@ -0,0 +1,14 @@
+import 'package:lib/study_lib.dart';
+
+class T3 extends ITextTest {
+  @override
+  String get authorName => 'BomI';
+
+  @override
+  void setInput(List<String> args) {}
+
+  @override
+  List<String> getOutput(Duration elapsed, Duration delta) {
+    throw UnimplementedError();
+  }
+}
